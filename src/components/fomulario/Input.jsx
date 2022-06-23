e877e337dbbfd94abfa7c39fcc59e767b834bcc9
Kershal <ericlisfds@hotmail.com>
2022-06-22 21:19:48 -0300

    até a aula 36

--- src/components/fomulario/Input.jsx
+++ src/components/fomulario/Input.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { useState } from "react";
+import "./input.css";
 
 export default function Input(props){
+    
+    const [valor, setValor] = useState("inicial");
+
+    function quandoMudar(e) {
+        
+        setValor(e.target.value);
+    }
+
     return (
-        <div>
-            Input
+        <div className="Input" >
+            <h2>{ valor }</h2>
+            <div style={{ display:"flex", flexDirection: "column"}}>
+                <input type="text" value={ valor } onChange={ quandoMudar } />
+                <input type="text" value={ valor } readOnly />
+                <input type="text" value={ undefined } />
+            </div>
         </div>
     );
 }
